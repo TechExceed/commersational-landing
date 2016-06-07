e76a55f750c13443783244702fb2e98bd90f0de1
yaronism <yaronism@gmail.com>
2016-06-07 13:22:30 +0300

    Update lead_mailer.rb
    Changed mail address to yaronism@gmail.com and also added Ilan to one of the emails.

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -4,16 +4,16 @@ class LeadMailer < ApplicationMailer
 
 	def lead_received_email(lead)
     @lead = lead
-    mail(to: ["ekedem@gmail.com", "ilan.kasan@gmail.com "], subject: 'Commersational - Lead received')
+    mail(to: ["yaronisn@gmail.com", "ilan.kasan@gmail.com "], subject: 'Commersational - Lead received')
   end
 
   def questionnaire_received_email(lead)
     @lead = lead
-    mail(to: ["ekedem@gmail.com", "ilan.kasan@gmail.com "], subject: 'Commersational - Questionnaire received')
+    mail(to: ["yaronism@gmail.com", "ilan.kasan@gmail.com "], subject: 'Commersational - Questionnaire received')
   end
 
   def lead_confirmation_email(lead)
   	@lead = lead
-  	mail(from: "\"Commersational\" <contact@commersational.com>", to: @lead.email, bcc: ["ekedem@gmail.com"], subject: 'Commersational - signup confirmation')
+  	mail(from: "\"Commersational\" <contact@commersational.com>", to: @lead.email, bcc: ["yaronism@gmail.com", "ilan.kasan@gmail.com "], subject: 'Commersational - signup confirmation')
   end
 end
